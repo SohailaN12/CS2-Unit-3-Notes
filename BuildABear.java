62aa77c9b3ffe94dd37a7949957998541b520b9b
SohailaN12 <sohaila.noori2027@gbwl.org>
2025-12-09 16:06:04 +0000

    3.3 notes

---
+++ BuildABear.java
@@ -0,0 +1,20 @@
+/**
+ * BuildABear.java defines the BuildABear CLASS 
+ * which is a template ofr BuildABear OBJECTS 
+ */
+public class BuildABear {
+    // 3 SECTION SIN AN OBJECT-CLASS!
+    // 1. INSTANCD VARIABLES (data/attributes/fields)
+    private String type; // just declare, don't assign
+    private String name;
+    private int fillLevel;
+    private  double price;
+    private boolean hasSpeaker;
+    private String recording;
+    // ENCAPSULATION: keep variables PRIVATE (they can't be dirctly accessed outside of this CLASS)
+
+
+    // 2. CONSTRUCTORS (initaliz values)
+
+    // 3. METHODS (behaviors/actions)
+}
--- Main.java
+++ Main.java
@@ -1,6 +1,19 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      // Create a new String -> SHORTCUT!
+      String lerbron = "GOAT";
+      // But String are actually OBJECTS 
+      // The original was to create String:
+      String steph = new String("GOAT")
+
+      // All objects can be created using this pattern:
+      // ClassName obiectName = new ClassName();
+      BuildABear alien = new BuildABear("alien");
+
+
+
+
+
    }
 }
