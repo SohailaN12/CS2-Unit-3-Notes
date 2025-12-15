23637cf8ac4c52dbcaf56230e678b48b17896234
SohailaN12 <sohaila.noori2027@gbwl.org>
2025-12-15 14:41:00 +0000

    3.4 notes

--- BuildABear.java
+++ BuildABear.java
@@ -6,15 +6,36 @@ public class BuildABear {
     // 3 SECTION SIN AN OBJECT-CLASS!
     // 1. INSTANCD VARIABLES (data/attributes/fields)
     private String type; // just declare, don't assign
-    private String name;
+    private String name; //"every BuildABear has a name..."
     private int fillLevel;
     private  double price;
     private boolean hasSpeaker;
     private String recording;
-    // ENCAPSULATION: keep variables PRIVATE (they can't be dirctly accessed outside of this CLASS)
-
-
+    // ENCAPSULATION: keep variables PRIVATE means 
+    // they cannot be directly accessed outside of 
+    // this Java CLASS (data is protected from users)
+    
     // 2. CONSTRUCTORS (initaliz values)
+    // DEFAULT CONSTRUCTOR does not take any arguments (no-arg)
+    // what gets called when user says "= new BuildABear()"
+    public BuildABear() {
+        this.type = "bear"; // WE PICKED the initial values!
+        this.name = "Teddy"; 
+        this.fillLevel = 75;
+        this.price = 150.00;
+        this.hasSpeaker = false;
+        this.recording = "";
+    }
+    // PARAMENTERIZE CONSTRUCTOR allows user to provide args
+    // user PASSES initial values to the constructor call
+    public BuildABear(String t, String n, int fl) {
+        this.type = t; // USER picked the value for type!
+        this.name = n;
+        this.fillLevel = fl;
+        this.price = 150.00;
+        this.hasSpeaker = false;
+        this.recording = "";
+    }
 
     // 3. METHODS (behaviors/actions)
 }
--- Main.java
+++ Main.java
@@ -2,14 +2,16 @@ public class Main {
 
    public static void main(String []args) {
       // Create a new String -> SHORTCUT!
-      String lerbron = "GOAT";
+      String lerbron = "PONY";
       // But String are actually OBJECTS 
       // The original was to create String:
-      String steph = new String("GOAT")
+      String steph = new String("pony");
 
       // All objects can be created using this pattern:
       // ClassName obiectName = new ClassName();
-      BuildABear alien = new BuildABear("alien");
+      BuildABear pony = new BuildABear();
+      // "BuildABear pony" is DECLARING a variable of type BuildABear
+      // "new BuildABear()" is CALLING a CONSTRUCTOR to set up the object
 
 
 
